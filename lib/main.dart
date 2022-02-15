9d71aa254ce3c483fd9561370b4c5f652aba722d
Gias Uddin Samir <giasuddin2548@gmail.com>
2022-02-15 17:27:25 +0600

    architecture added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:bloc_state_management/views/home_bloc/home_bloc.dart';
-import 'package:bloc_state_management/views/home_view.dart';
+
+import 'package:bloc_state_management/presentation_layer/screens/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -12,19 +12,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-        providers: [
-          BlocProvider<HomeBloc>(
-            create: (BuildContext context) => HomeBloc(),
-          ),
-        ],
-        child: MaterialApp(
+    return
+      // MultiBlocProvider(
+      //   providers: [
+      //     BlocProvider<HomeBloc>(
+      //       create: (BuildContext context) => HomeBloc(),
+      //     ),
+      //   ],
+      //   child:
+          MaterialApp(
+            debugShowCheckedModeBanner: false,
           title: 'Flutter Demo',
           theme: ThemeData(
             primarySwatch: Colors.blue,
           ),
-          home:  HomeView(),
-        ));
+          home:  HomeScreen(),
+        // )
+    );
   }
 }
 
